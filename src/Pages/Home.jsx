eeca5898f8c0f81e2088d3775c697e4c82ac07b5
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-01-25 19:15:27 +0530

    Move the pages page out of components and also add the Footer Home Page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='flex justify-center text-2xl text-blue-400 m-8'>
+          
+      </h1>
+    </div>
+  )
+}
